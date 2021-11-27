c69ce070500cbeaa92104d64f566be5f9dcea976
Prakhar <prakhsri98@gmail.com>
2021-11-27 18:54:40 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+    name='my_env',  # Use same name as sibling directory.
+    version='1.0.0',  # Version number.
+    install_requires=['gym']  # Python packages required by our environments.
+)
